f55f66bbe840e56ec168d633f4775407a5ea7fe9
AndyZakk <AndyZakk@mail.ru>
2019-03-30 00:24:08 -0400

    nothing changed, just code formating

--- model.py
+++ model.py
@@ -5,30 +5,30 @@ import sys as sys
 import os
 import matplotlib as mpl
 
-SPAM=[]
-HAM=[]
-STOPWORDS =[]
+SPAM = []
+HAM = []
+STOPWORDS = []
 
 
-#~~~~~~~ HELPER METHOD ~~~~~~~
+# ~~~~~~~ HELPER METHOD ~~~~~~~
 def process_stop_word(file):
     words = open(file, "r")
     for word in words:
         STOPWORDS.append(word)
 
 
-#!!!!!!!!!!NOT DONE!!!!!!!!!!!!#
+# !!!!!!!!!!NOT DONE!!!!!!!!!!!!#
 def build_vocabulary(category):
     for document in category:
         path = "train\\"
         path += document
         print(path)
-        data = open(path,"r")
+        data = open(path, "r")
         for line in data:
-            toPrint = line.lower()
-            toPrint = re.split('[^a-zA-Z]+', toPrint)
-            print(toPrint)
-            
+            to_print = line.lower()
+            to_print = re.split('[^a-zA-Z]+', to_print)
+            print(to_print)
+
 
 def process_files(folder):
     """
@@ -36,13 +36,14 @@ def process_files(folder):
     attributes:
     - folder: a folder from where files should be read and processed
     """
-    #store all the files from the foler in a list for further processing
+    # store all the files from the foler in a list for further processing
     files = []
     for i in os.listdir(folder):
-            files.append(i)
-    #process each file from the list of files and store into its designated place
+        files.append(i)
+    # process each file from the list of files and store into its designated place
     for document in files:
         categorize(document)
+
 
 def categorize(file):
     """
@@ -50,15 +51,12 @@ def categorize(file):
     attributes:
     - file: given file to process
     """
-    spam_filter = re.compile("spam",)   #re.compile("spam")
+    spam_filter = re.compile("spam", )  # re.compile("spam")
     if spam_filter.search(file) is None:
         HAM.append(file)
     else:
         SPAM.append(file)
 
 
-
 process_files("train")
 build_vocabulary(SPAM)
-   
-    
